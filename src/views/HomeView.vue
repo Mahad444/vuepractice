36d47fb51f946a014d0e60145d91ca061ac19d4c
Mahad444 <mahadsaid95@icloud.com>
2023-04-04 23:19:46 +0300

    Starting Vue learning child components

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+//import TheWelcome from '../components/TheWelcome.vue'
+//import ChildComponent from '../components/ChildComponent.vue';
+import ChildComponent from '../components/ChildComponent.vue';
+export default{
+   // component: { ChildComponent },
+    components: { ChildComponent }
+}
+</script>
+
+<template>
+  <main>
+    <ChildComponent/>
+  </main>
+</template>
