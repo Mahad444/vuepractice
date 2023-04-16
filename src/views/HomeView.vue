2e342371e8b0e6c1718c8a05606e2c7ccf4360ca
Mahad444 <mahadsaid95@icloud.com>
2023-04-16 23:30:39 +0300

    removing the welcome Component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script>
-//import TheWelcome from '../components/TheWelcome.vue'
 //import ChildComponent from '../components/ChildComponent.vue';
 import ChildComponent from '../components/ChildComponent.vue';
 export default{
-   // component: { ChildComponent },
-    components: { ChildComponent }
+   component: { ChildComponent }
+    // components: { ChildComponent }
 }
 </script>
 
